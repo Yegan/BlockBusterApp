bd786477a586f515d59a9b67ad664d7bf5a11de2
Yegan <yeganj@gmail.com>
2019-10-17 19:30:41 +0200

    first commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vidily.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movie = new MoviesController();
+            return View();
+        }
+    }
+}
